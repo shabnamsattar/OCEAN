a47edf0dc4fe3c1993aa535859c90a9e8903ce2f
Shabnam Sattar <165808527+shabnamsattar@users.noreply.github.com>
2025-07-26 19:40:18 +0200

    Update RandomAndIsolationForest.py

--- src/RandomAndIsolationForest.py
+++ src/RandomAndIsolationForest.py
@@ -1,22 +1,27 @@
 class RandomAndIsolationForest:
-    def __init__(self, randomForest, isolationForest=None):
+    def __init__(self, randomForest=None, isolationForest=None):
         """
         Combines a randomForest and an isolationForest inputs into
         a completeForest object.
         """
         self.randomForest = randomForest
         self.isolationForest = isolationForest
-        # Store the completeForest size, the indices of the subforests
-        # and the tree estimators.
-        self.n_estimators = randomForest.n_estimators
-        self.randomForestEstimatorsIndices = list(
-            range(randomForest.n_estimators))
-        self.estimators_ = [est for est in randomForest.estimators_]
-        # Add the isolation forest to the completeForest if input non empty
+
+        self.estimators_ = []
+        self.randomForestEstimatorsIndices = []
+        self.isolationForestEstimatorsIndices = []
+
+        self.n_estimators = 0
+
+        if randomForest:
+            self.n_estimators += randomForest.n_estimators
+            self.randomForestEstimatorsIndices = list(range(randomForest.n_estimators))
+            self.estimators_ += randomForest.estimators_
+
         if isolationForest:
+            offset = len(self.estimators_)
             self.n_estimators += isolationForest.n_estimators
             self.isolationForestEstimatorsIndices = [
-                i + randomForest.n_estimators
-                for i in range(isolationForest.n_estimators)]
-            for est in isolationForest.estimators_:
-                self.estimators_.append(est)
+                i + offset for i in range(isolationForest.n_estimators)
+            ]
+            self.estimators_ += isolationForest.estimators_
