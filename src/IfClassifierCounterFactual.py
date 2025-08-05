9608014da8ecb9f54e1c4b2df02c875dffa063a5
Shabnam Sattar <165808527+shabnamsattar@users.noreply.github.com>
2025-08-05 18:01:52 +0200

    Update IfClassifierCounterFactual.py

--- src/IfClassifierCounterFactual.py
+++ src/IfClassifierCounterFactual.py
@@ -2,6 +2,7 @@ import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
 from sklearn.ensemble._iforest import _average_path_length
+import math
 
 from src.ClassifierCounterFactual import ClassifierCounterFactualMilp
 from src.RandomForestCounterfactual import RandomForestCounterfactualMilp
