b2a599f95e5816abba7ce0c1605f781996a2d324
Shabnam Sattar <165808527+shabnamsattar@users.noreply.github.com>
2025-10-20 12:02:02 +0200

    Update IfCounterfactual.py

--- src/IfCounterfactual.py
+++ src/IfCounterfactual.py
@@ -64,7 +64,7 @@ class IfCounterfactualMilp(CounterfactualMilp, RandomForestCounterfactualMilp):
 
     # ---------- Key new constraint: log2 anomaly score ----------
     def __addAnomalyScoreConstraint(self, threshold=0.0):
-        
+        print("hi")
         expr = gp.LinExpr(0.0)  # will hold the average depth E[h]
         for t in self.completeForest.isolationForestEstimatorsIndices:
             tm   = self.treeManagers[t]
@@ -86,6 +86,7 @@ class IfCounterfactualMilp(CounterfactualMilp, RandomForestCounterfactualMilp):
 
     # ---------- Build & Solve ----------
     def buildModel(self, decision_threshold=0.0):
+        print("build11")
         # variables & per-feature feasible sets
         self.initSolution()
         # build per-tree path variables & inter-tree consistency
@@ -100,8 +101,10 @@ class IfCounterfactualMilp(CounterfactualMilp, RandomForestCounterfactualMilp):
         
         # objective (closest x' to x0)
         self.initObjective()  # from CounterfactualMilp
+        print("build12")
 
     def solveModel(self, time_limit=600, threads=4):
+        print("startsolve1")
         self.model.setParam(GRB.Param.TimeLimit, time_limit)
         self.model.setParam(GRB.Param.Threads, threads)
         self.model.optimize()
@@ -109,4 +112,5 @@ class IfCounterfactualMilp(CounterfactualMilp, RandomForestCounterfactualMilp):
             self.x_sol = self.x0
             return False
         self.x_sol = [[self.x_var_sol[f].getAttr(GRB.Attr.X) for f in range(self.nFeatures)]]
+        print("startsolve2")
         return True
